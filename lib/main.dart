ddbde179f9c670ba6726eba569b1f4ed7d7fa49c
Shogo Takegawa <b6151@oic.jp>
2020-12-05 20:25:05 +0900

    container
    https://www.flutter-study.dev/widgets/container-widget

--- lib/main.dart
+++ lib/main.dart
@@ -22,9 +22,27 @@ class MyApp extends StatelessWidget {
             Text('data', style: TextStyle(fontSize: 100)),
             Container(
               width: double.infinity,
-              color: Colors.grey,
               // 表示位置を指定
-              child: Text('TextAlign.right', textAlign: TextAlign.right),
+              child: Text(
+                'TextAlign.right',
+                textAlign: TextAlign.right,
+              ),
+              padding: EdgeInsets.all(8),
+              margin: EdgeInsets.all(19),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.deepPurpleAccent, width: 3),
+                  borderRadius: BorderRadius.circular(700), // 上限なんぼ？
+                  color: Colors.blue),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage('https://placehold.jp/200x100.png'),
+                ),
+              ),
+              child: Text('Image'),
+              width: 200,
+              height: 100,
             ),
           ],
         ),
